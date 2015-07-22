273c08154ba8ef7ecfa58652c2754cb401571833
Roy Menczer <roym@mellanox.com>
2015-07-22 17:28:02 +0300

    Create code.c file
    code.c file will eventually contain code for server and client
    currently contains "#define"s, an #include, and an empty main function (plus one line in server_func)
    code compiles with gcc -g -O0

---
+++ code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "1771" //arbitrary port number
+
+
+int main() //FIXME
+{
+	return 0;	
+}
+// --------- THE server.c CODE ---------
+
+int server_func()
+{
+	int listen_sock, new_fd;
+	return 0;
+}
+
+// --------- THE client.c CODE ----------
+
+
